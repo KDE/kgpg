2b8bff0e128ab253d9a51fb8da8e3b91208821e8
Jean-Baptiste Mardelle <jb@kdenlive.org>
2002-11-25 22:33:06 +0000

    forgot to commit before
    svn path=/trunk/kdeutils/kgpg/; revision=190482

---
+++ kgpgoption.ui.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename slots use Qt Designer which will
+** update this file, preserving your code. Create an init() slot in place of
+** a constructor, and a destroy() slot in place of a destructor.
+*****************************************************************************/
+#include <kcombobox.h>
+#include <kurlrequester.h>
+
+void KOptions::file_2_2_toggled( bool ison)
+{
+filekey_2_2->setEnabled(ison);
+}
+
+
+void KOptions::defaut_2_2_toggled( bool ison)
+{
+defautkey_2_2->setEnabled(ison);
+}
+
+
+
