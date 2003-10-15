1195679f62405445d29f64f20ff38be5a3395912
Jean-Baptiste Mardelle <jb@kdenlive.org>
2003-10-15 12:22:31 +0000

    fix toolbar disappearing
    svn path=/trunk/kdeutils/kgpg/; revision=259135

--- kgpg.cpp
+++ kgpg.cpp
@@ -718,7 +718,7 @@ KgpgAppletApp::~KgpgAppletApp()
 void KgpgAppletApp::slotHandleQuit()
 {
 s_keyManager->keysList2->saveLayout(KGlobal::config(),"KeyView");
-        KGpgSettings::setShowToolbar(s_keyManager->toolBar()->isVisible());
+        KGpgSettings::setShowToolbar(!s_keyManager->toolBar()->isHidden());
         KGpgSettings::setPhotoProperties(s_keyManager->photoProps->currentItem());
 	KGpgSettings::setShowTrust(s_keyManager->sTrust->isChecked());
 	KGpgSettings::setShowExpi(s_keyManager->sExpi->isChecked());
