1ab625fc3ebf4b942806561e2e291fe11af18485
Jimmy Gilles <jimmygilles@gmail.com>
2006-07-15 15:35:01 +0000

    nomsg
    svn path=/trunk/KDE/kdeutils/kgpg/; revision=562725

--- selectsecretkey.cpp
+++ selectsecretkey.cpp
@@ -21,11 +21,11 @@
 #include "selectsecretkey.h"
 
 KgpgSelectSecretKey::KgpgSelectSecretKey(QWidget *parent, const bool &signkey, const int &countkey)
-    : KDialog(parent)
+                   : KDialog(parent)
 {
-    setCaption( i18n("Private Key List") );
-    setButtons( Ok | Cancel);
-    setDefaultButton( Ok );
+    setCaption(i18n("Private Key List"));
+    setButtons(Ok | Cancel);
+    setDefaultButton(Ok);
     QWidget *page = new QWidget(this);
 
     KIconLoader *loader = KGlobal::iconLoader();
