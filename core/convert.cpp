0c42998ed6f45985938436cf0290426d11e87c87
Rolf Eike Beer <kde@opensource.sf-tec.de>
2007-08-16 11:06:12 +0000

    convert multi-if to switch
    svn path=/trunk/KDE/kdeutils/kgpg/; revision=700750

--- core/convert.cpp
+++ core/convert.cpp
@@ -28,90 +28,63 @@
 namespace KgpgCore
 {
 
-QString Convert::toString(const KgpgKeyAlgo &algorithme)
+QString Convert::toString(const KgpgKeyAlgo &algorithm)
 {
-    if (algorithme == ALGO_UNKNOWN)
-        return i18n("Unknown");
-    if (algorithme == ALGO_RSA)
-        return QString("RSA");
-    if (algorithme == ALGO_DSA)
-        return QString("DSA");
-    if (algorithme == ALGO_ELGAMAL)
-        return QString("ElGamal");
-    if (algorithme == ALGO_DSA_ELGAMAL)
-        return QString("DSA - ElGamal");
-
-    return i18n("Unknown");
+	switch (algorithm) {
+	case ALGO_UNKNOWN:	return i18n("Unknown");
+	case ALGO_RSA:		return QString("RSA");
+	case ALGO_DSA:		return QString("DSA");
+	case ALGO_ELGAMAL:	return QString("ElGamal");
+	case ALGO_DSA_ELGAMAL:	return QString("DSA - ElGamal");
+	default:		return i18n("Unknown");
+	}
 }
 
 QString Convert::toString(const KgpgKeyOwnerTrust &ownertrust)
 {
-    if (ownertrust == OWTRUST_UNKNOWN)
-        return i18n("Unknown");
-    if (ownertrust == OWTRUST_UNDEFINED)
-        return i18n("Do not Know");
-    if (ownertrust == OWTRUST_NONE)
-        return i18n("Do NOT Trust");
-    if (ownertrust == OWTRUST_MARGINAL)
-        return i18n("Marginally");
-    if (ownertrust == OWTRUST_FULL)
-        return i18n("Fully");
-    if (ownertrust == OWTRUST_ULTIMATE)
-        return i18n("Ultimately");
-
-    return i18n("Unknown");
+	switch (ownertrust) {
+	case OWTRUST_UNKNOWN:	return i18n("Unknown");
+	case OWTRUST_UNDEFINED:	return i18n("Do not Know");
+	case OWTRUST_NONE:	return i18n("Do NOT Trust");
+	case OWTRUST_MARGINAL:	return i18n("Marginally");
+	case OWTRUST_FULL:	return i18n("Fully");
+	case OWTRUST_ULTIMATE:	return i18n("Ultimately");
+	default:		return i18n("Unknown");
+	}
 }
 
 QString Convert::toString(const KgpgKeyTrust &trust)
 {
-    if (trust == TRUST_UNKNOWN)
-        return i18n("Unknown");
-    if (trust == TRUST_INVALID)
-        return i18n("Invalid");
-    if (trust == TRUST_DISABLED)
-        return i18n("Disabled");
-    if (trust == TRUST_REVOKED)
-        return i18n("Revoked");
-    if (trust == TRUST_EXPIRED)
-        return i18n("Expired");
-    if (trust == TRUST_UNDEFINED)
-        return i18n("Undefined");
-    if (trust == TRUST_NONE)
-        return i18n("None");
-    if (trust == TRUST_MARGINAL)
-        return i18n("Marginal");
-    if (trust == TRUST_FULL)
-        return i18n("Full");
-    if (trust == TRUST_ULTIMATE)
-        return i18n("Ultimate");
-
-    return i18n("Unknown");
+	switch (trust) {
+	case TRUST_UNKNOWN:	return i18n("Unknown");
+	case TRUST_INVALID:	return i18n("Invalid");
+	case TRUST_DISABLED:	return i18n("Disabled");
+	case TRUST_REVOKED:	return i18n("Revoked");
+	case TRUST_EXPIRED:	return i18n("Expired");
+	case TRUST_UNDEFINED:	return i18n("Undefined");
+	case TRUST_NONE:	return i18n("None");
+	case TRUST_MARGINAL:	return i18n("Marginal");
+	case TRUST_FULL:	return i18n("Full");
+	case TRUST_ULTIMATE:	return i18n("Ultimate");
+	default:		return i18n("Unknown");
+	}
 }
 
 QColor Convert::toColor(const KgpgKeyTrust &trust)
 {
-    if (trust == TRUST_UNKNOWN)
-        return KGpgSettings::colorUnknown();
-    if (trust == TRUST_INVALID)
-        return KGpgSettings::colorBad();
-    if (trust == TRUST_DISABLED)
-        return KGpgSettings::colorBad();
-    if (trust == TRUST_REVOKED)
-        return KGpgSettings::colorRev();
-    if (trust == TRUST_EXPIRED)
-        return KGpgSettings::colorBad();
-    if (trust == TRUST_UNDEFINED)
-        return KGpgSettings::colorUnknown();
-    if (trust == TRUST_NONE)
-        return KGpgSettings::colorUnknown();
-    if (trust == TRUST_MARGINAL)
-        return KGpgSettings::colorBad();
-    if (trust == TRUST_FULL)
-        return KGpgSettings::colorGood();
-    if (trust == TRUST_ULTIMATE)
-        return KGpgSettings::colorGood();
-
-    return KGpgSettings::colorUnknown();
+	switch (trust) {
+	case TRUST_UNKNOWN:	return KGpgSettings::colorUnknown();
+	case TRUST_INVALID:	return KGpgSettings::colorBad();
+	case TRUST_DISABLED:	return KGpgSettings::colorBad();
+	case TRUST_REVOKED:	return KGpgSettings::colorRev();
+	case TRUST_EXPIRED:	return KGpgSettings::colorBad();
+	case TRUST_UNDEFINED:	return KGpgSettings::colorUnknown();
+	case TRUST_NONE:	return KGpgSettings::colorUnknown();
+	case TRUST_MARGINAL:	return KGpgSettings::colorBad();
+	case TRUST_FULL:	return KGpgSettings::colorGood();
+	case TRUST_ULTIMATE:	return KGpgSettings::colorGood();
+	default:		return KGpgSettings::colorUnknown();
+	}
 }
 
 QString Convert::toString(const QDate &date)
