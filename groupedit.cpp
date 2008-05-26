7ba62757d22e5d37845ede2596bdb7ae8311e119
Rolf Eike Beer <kde@opensource.sf-tec.de>
2008-05-26 12:31:30 +0000

    add missing license statements
    svn path=/trunk/KDE/kdeutils/kgpg/; revision=812796

--- groupedit.cpp
+++ groupedit.cpp
@@ -1,8 +1,21 @@
+/**
+ * Copyright (C) 2002 Jean-Baptiste Mardelle <bj@altern.org>
+ * Copyright (C) 2007,2008 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ */
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "groupedit.h"
 #include <QHeaderView>
 #include "groupeditproxymodel.h"
 #include "kgpgitemmodel.h"
-
 
 groupEdit::groupEdit(QWidget *parent, QList<KGpgNode *> *ids)
 	: QWidget( parent ), members(ids)
--- groupedit.h
+++ groupedit.h
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2002 Jean-Baptiste Mardelle <bj@altern.org>
+ * Copyright (C) 2007,2008 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ */
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef GROUPEDIT_H
 #define GROUPEDIT_H
 
