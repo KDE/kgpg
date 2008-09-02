3b4a93698ec956d37ffd99e5f3092b2ba95a7833
Rolf Eike Beer <kde@opensource.sf-tec.de>
2008-09-02 21:34:22 +0000

    loading photo ids now also works on Windows
    svn path=/trunk/KDE/kdeutils/kgpg/; revision=856456

--- kgpginterface.cpp
+++ kgpginterface.cpp
@@ -1078,7 +1078,11 @@ void KgpgInterface::signKeyOpenConsole()
 
 QPixmap KgpgInterface::loadPhoto(const QString &keyid, const QString &uid, const bool &block)
 {
-	QString pgpgoutput = "cat %i";
+#if Q_OS_WIN32
+	QString pgpgoutput("cmd /C \"echo %I\"");
+#else
+	QString pgpgoutput("echo %I");
+#endif
 
 	m_workProcess = new KProcess(this);
 	m_workProcess->setOutputChannelMode(KProcess::OnlyStdoutChannel);
@@ -1102,7 +1106,15 @@ void KgpgInterface::loadPhotoFin(int exitCode)
 
 	if (exitCode == 0) {
 		QByteArray pic = m_workProcess->readAllStandardOutput();
-		m_pixmap.loadFromData(pic);
+
+		while (pic.endsWith('\r') || pic.endsWith('\n'))
+			pic.remove(pic.length() - 1, 1);
+
+		KUrl url(pic);
+		m_pixmap.load(url.path());
+		QFile::remove(url.path());
+		QDir dir;
+		dir.rmdir(url.directory());
 	}
 
 	emit loadPhotoFinished(m_pixmap, this);
