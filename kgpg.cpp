8d044641899c0519fc56f5381983b767d7756c55
Rolf Eike Beer <kde@opensource.sf-tec.de>
2009-09-09 20:04:39 +0000

    make "kgpg -d" work again if systray is disabled
    Forward-port of r1020953
    
    svn path=/trunk/KDE/kdeutils/kgpg/; revision=1021686

--- kgpg.cpp
+++ kgpg.cpp
@@ -100,7 +100,7 @@ int KGpgApp::newInstance()
 	w->goDefaultKey = goHome;
 
 	// parsing of command line args
-	if (args->isSet("k") || (!KGpgSettings::showSystray() && (args->count() == 0))) {
+	if (args->isSet("k") || (!KGpgSettings::showSystray() && (args->count() == 0) && !args->isSet("d"))) {
 		s_keyManager->show();
 		KWindowSystem::setOnDesktop(s_keyManager->winId(), KWindowSystem::currentDesktop());  //set on the current desktop
 		KWindowSystem::unminimizeWindow(s_keyManager->winId());  //de-iconify window
