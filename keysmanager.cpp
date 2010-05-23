90c4bfbfb230a1f78beb6b1f1ff5bfb696c4b191
Rolf Eike Beer <kde@opensource.sf-tec.de>
2010-05-23 17:53:27 +0000

    don't crash if the first time assistant tells us a key id we currently do not have loaded
    BUG:238517
    
    svn path=/branches/KDE/4.4/kdeutils/kgpg/; revision=1129769

--- keysmanager.cpp
+++ keysmanager.cpp
@@ -1279,6 +1279,12 @@ void KeysManager::slotSetDefaultKey(const QString &newID)
 {
 	KGpgKeyNode *ndef = imodel->getRootNode()->findKey(newID);
 
+	if (ndef == NULL) {
+		KGpgSettings::setDefaultKey(newID);
+		KGpgSettings::self()->writeConfig();
+		return;
+	}
+
 	setDefaultKeyNode(ndef);
 }
 
