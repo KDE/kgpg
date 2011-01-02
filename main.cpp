c2fe7c1018cddc3cf3e0a72d91f770ec091f1cd6
Rolf Eike Beer <kde@opensource.sf-tec.de>
2011-01-02 14:43:12 +0000

    bump version number for RC2
    svn path=/branches/KDE/4.6/kdeutils/kgpg/; revision=1210875

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@
 static const char description[] =
         I18N_NOOP("KGpg - simple gui for gpg\n\nKGpg was designed to make gpg very easy to use.\nI tried to make it as secure as possible.\nHope you enjoy it.");
 
-static const char version[] = "2.4.60";
+static const char version[] = "2.4.92";
 
 int main(int argc, char *argv[])
 {
