a2b5527f886634a127e529fe430a593cdb75f413
Burkhard Lück <lueck@hube-lueck.de>
2011-05-31 20:54:18 +0000

    make EMailTemplate string translatable CCMAIL:kde-i18n-doc@kde.org BUG:272637
    svn path=/branches/KDE/4.6/kdeutils/kgpg/; revision=1234363

--- kgpg.h
+++ kgpg.h
@@ -21,11 +21,23 @@
 #include <KUniqueApplication>
 #include <KShortcut>
 #include <KUrl>
+#include <klocale.h>
 
 class KCmdLineArgs;
 class KeysManager;
 class KGpgExternalActions;
 class QString;
+
+static const char * const EMailTemplateText=I18N_NOOP(
+    "Hi,\n\nplease find attached the user id '%UIDNAME%' of your key %KEYID% signed by me. "
+    "This mail is encrypted with that key to make sure you control both the email address and the key.\n\n"
+    "If you have multiple user ids, I sent the signature for each user id separately to that user id's associated email address. "
+    "You can import the signatures by running each through `gpg --import` after you have decrypted them with `gpg --decrypt`.\n\n"
+    "If you are using KGpg store the attachment to disk and then import it. Just select `Import Key...` from `Keys` menu and open the file.\n\n"
+    "Note that I did not upload your key to any keyservers. If you want this new signature to be available to others, please upload it yourself. "
+    "With GnuPG this can be done using gpg --keyserver subkeys.pgp.net --send-key %KEYID%.\n\n" 
+    "With KGpg you can right click on the key once you imported all user ids and choose `Export Public Key...`.\n\n"
+    "If you have any questions, don't hesitate to ask.\n");
 
 class KGpgApp : public KUniqueApplication
 {
