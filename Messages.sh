d8c66f1b9b9486cceb7bbc8102aca5e4d03fa14c
Burkhard Lück <lueck@hube-lueck.de>
2011-05-31 20:55:22 +0000

    port from branch r1234363: make EMailTemplate string translatable CCBUG:272637
    svn path=/trunk/KDE/kdeutils/kgpg/; revision=1234364

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 $PREPARETIPS > tips.cpp
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT *.cpp core/*.cpp editor/*.cpp model/*.cpp transactions/*.cpp -o $podir/kgpg.pot
+$XGETTEXT *.h *.cpp core/*.cpp editor/*.cpp model/*.cpp transactions/*.cpp -o $podir/kgpg.pot
 rm -f tips.cpp
