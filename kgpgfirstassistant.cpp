7d58ee47f10f6457b3ca4a1f3ec4c1cebacec0e2
Rolf Eike Beer <kde@opensource.sf-tec.de>
2012-03-11 11:08:22 +0100

    improve selection of GnuPG binary
    Search for the full path of the binary, this will avoid scanning through the
    path on every invocation. Also prefer the "gpg2" binary over the "gpg" one as
    it has the more modern interface.

--- kgpgfirstassistant.cpp
+++ kgpgfirstassistant.cpp
@@ -26,6 +26,7 @@
 #include <KComboBox>
 #include <KLocale>
 #include <KMessageBox>
+#include <KStandardDirs>
 #include <KUrlRequester>
 #include <QCheckBox>
 #include <QGridLayout>
@@ -107,10 +108,15 @@ KGpgFirstAssistant::KGpgFirstAssistant(QWidget *parent)
 
 	binURL = new KUrlRequester(page);
 	binURL->setFilter(i18nc("search filter for gpg binary", "gpg|GnuPG binary\n*|All files"));
-	binURL->setUrl(KUrl("gpg"));
+	QString gpgBin = KStandardDirs::findExe(QLatin1String("gpg2"));
+	if (gpgBin.isEmpty())
+		gpgBin = KStandardDirs::findExe(QLatin1String("gpg"));
+	if (gpgBin.isEmpty())
+		gpgBin = QLatin1String("gpg");
+	binURL->setUrl(KUrl::fromPath(gpgBin));
 
 	connect(binURL, SIGNAL(textChanged(QString)), SLOT(slotBinaryChanged(QString)));
-	slotBinaryChanged(QLatin1String( "gpg" ));
+	slotBinaryChanged(gpgBin);
 
 	gridLayout->addWidget(binURL, 2, 1, 1, 1);
 
