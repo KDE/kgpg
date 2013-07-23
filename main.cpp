96a2c2ecc1e66b8d0cde116f8203ff1216b93b23
Rolf Eike Beer <kde@opensource.sf-tec.de>
2013-07-23 08:13:22 +0200

    bump version number for KDE SC 4.11rc2

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@
 static const char description[] =
         I18N_NOOP("KGpg - simple gui for gpg\n\nKGpg was designed to make gpg very easy to use.\nI tried to make it as secure as possible.\nHope you enjoy it.");
 
-static const char version[] = "2.9.90";
+static const char version[] = "2.9.97";
 
 int main(int argc, char *argv[])
 {
