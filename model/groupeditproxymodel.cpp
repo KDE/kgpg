5b14e435297d8c73face6579e56aa156bb621244
Rolf Eike Beer <kde@opensource.sf-tec.de>
2014-10-19 21:00:18 +0200

    remove unused method GroupEditProxyModel::setMinTrust()

--- model/groupeditproxymodel.cpp
+++ model/groupeditproxymodel.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2008,2010,2012,2013  Rolf Eike Beer <kde@opensource.sf-tec.de>
+/* Copyright 2008,2010,2012,2013,2014  Rolf Eike Beer <kde@opensource.sf-tec.de>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -39,13 +39,6 @@ GroupEditProxyModel::setKeyModel(KGpgItemModel *md)
 {
 	m_model = md;
 	setSourceModel(md);
-}
-
-void
-GroupEditProxyModel::setMinTrust(const KgpgCore::KgpgKeyTrust mintrust)
-{
-	m_mintrust = mintrust;
-	invalidate();
 }
 
 bool
