fb19c2df86003509adf9abed6de213a2206fb120
Rolf Eike Beer <kde@opensource.sf-tec.de>
2014-10-19 22:48:25 +0200

    show a warning icon and a tooltip for expired group member nodes
    CCBUG:339749

--- model/groupeditproxymodel.cpp
+++ model/groupeditproxymodel.cpp
@@ -22,6 +22,8 @@
 #include "core/kgpgkey.h"
 
 #include <KDebug>
+#include <KLocale>
+#include <QIcon>
 
 using namespace KgpgCore;
 
@@ -91,23 +93,39 @@ GroupEditProxyModel::data(const QModelIndex &index, int role) const
 	if (!index.isValid() || (index.column() >= 3))
 		return QVariant();
 
-	if ((role != Qt::ToolTipRole) && (role != Qt::DisplayRole))
-		return QVariant();
-
 	KGpgNode *nd = m_model->nodeForIndex(mapToSource(index));
-	switch (index.column()) {
-	case 0:
-		if (role == Qt::ToolTipRole)
-			return nd->getNameComment();
-		else
-			return nd->getName();
-	case 1:
-		return nd->getEmail();
-	case 2:
-		if (role == Qt::ToolTipRole)
-			return nd->toKeyNode()->getBeautifiedFingerprint();
-		else
-			return nd->getId().right(8);
+
+	switch (role) {
+	case Qt::ToolTipRole:
+	case Qt::DisplayRole:
+		switch (index.column()) {
+		case 0:
+			if (role == Qt::ToolTipRole)
+				return nd->getNameComment();
+			else
+				return nd->getName();
+		case 1:
+			if (role == Qt::ToolTipRole) {
+				if (nd->toKeyNode()->getExpiration().isValid() && (nd->toKeyNode()->getExpiration() <= QDateTime::currentDateTime()))
+					return i18nc("Expired key", "Expired");
+				break;
+			} else {
+				return nd->getEmail();
+			}
+		case 2:
+			if (role == Qt::ToolTipRole)
+				return nd->toKeyNode()->getBeautifiedFingerprint();
+			else
+				return nd->getId().right(8);
+		default:
+			break;
+		}
+	case Qt::DecorationRole:
+		if (index.column() != 1)
+			break;
+
+		if (nd->toKeyNode()->getExpiration().isValid() && (nd->toKeyNode()->getExpiration() <= QDateTime::currentDateTime()))
+			return QIcon::fromTheme(QLatin1String("dialog-warning"));
 	}
 
 	return QVariant();
