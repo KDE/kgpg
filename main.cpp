d0ea892b46dd4a48116a0b91a2daf3de7d5926f4
Rolf Eike Beer <kde@opensource.sf-tec.de>
2015-03-25 21:39:33 +0100

    bump version number for KDE Applications 15.04 rc1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2002 Jean-Baptiste Mardelle <bj@altern.org>
- * Copyright (C) 2007,2008,2009,2010,2011,2012,2013,2014
+ * Copyright (C) 2007,2008,2009,2010,2011,2012,2013,2014,2015
  *               Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
@@ -23,7 +23,7 @@
 static const char description[] =
         I18N_NOOP("KGpg - simple gui for gpg\n\nKGpg was designed to make gpg very easy to use.\nI tried to make it as secure as possible.\nHope you enjoy it.");
 
-static const char version[] = "2.14.40";
+static const char version[] = "2.14.97";
 
 int main(int argc, char *argv[])
 {
