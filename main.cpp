0e074858195a306aa99a42557161dd74f62872bd
Rolf Eike Beer <kde@opensource.sf-tec.de>
2016-01-04 21:06:56 +0100

    bump version number for post Applications 15.12 development

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2002 Jean-Baptiste Mardelle <bj@altern.org>
- * Copyright (C) 2007,2008,2009,2010,2011,2012,2013,2014,2015
+ * Copyright (C) 2007,2008,2009,2010,2011,2012,2013,2014,2015,2016
  *               Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
@@ -23,7 +23,7 @@
 static const char description[] =
         I18N_NOOP("KGpg - simple gui for gpg\n\nKGpg was designed to make gpg very easy to use.\nI tried to make it as secure as possible.\nHope you enjoy it.");
 
-static const char version[] = "2.17.0";
+static const char version[] = "2.17.40";
 
 int main(int argc, char *argv[])
 {
