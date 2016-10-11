b726585ae2efe39b4497842638062e64f2d6046c
Rolf Eike Beer <eb@emlix.com>
2016-10-11 12:14:27 +0200

    drop needless friend declaration, KGpgSubkeyNode is a subclass

--- core/KGpgExpandableNode.h
+++ core/KGpgExpandableNode.h
@@ -21,7 +21,6 @@
 
 #include "KGpgNode.h"
 
-class KGpgSubkeyNode;
 class KGpgRefNode;
 
 /**
@@ -36,7 +35,6 @@ class KGpgExpandableNode : public KGpgNode
 	Q_OBJECT
 
 	friend class KGpgRefNode;
-	friend class KGpgSubkeyNode;
 protected:
 	KGpgNode::List children;
 
