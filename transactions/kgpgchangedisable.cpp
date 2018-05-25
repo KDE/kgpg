KDE/kgpg

commit 68fc9165947c054423ac600b11462294d56fa06e

Resolve KGpg crashing when setting trust level to ultimate
KGpg with GnuPG 2.2 crashes when setting trust level of a imported key to ultimate.

BUGS:373909,391499
Differential Revision: https://phabricator.kde.org/D12624

transactions/kgpgchangedisable.cpp
@@ -45,12 +45,3 @@ KGpgChangeDisable::preStart()
 
 	return true;
 }
-
-bool
-KGpgChangeDisable::nextLine(const QString &line)
-{
-	if(keyConsidered(line, QStringList(getKeyid())))
-		return false;
-
-	return KGpgEditKeyTransaction::nextLine(line);
-}

transactions/kgpgdelkey.cpp
@@ -52,9 +52,7 @@ KGpgDelKey::~KGpgDelKey()
 bool
 KGpgDelKey::nextLine(const QString &line)
 {
-	if (keyConsidered(line, fingerprints)) {
-		// nothing
-	} else if (!line.startsWith(QLatin1String("[GNUPG:] GOT_IT")))
+	if (!line.startsWith(QLatin1String("[GNUPG:] GOT_IT")))
 		setSuccess(KGpgTransaction::TS_MSG_SEQUENCE);
 
 	return false;

transactions/kgpgtransactionprivate.cpp
@@ -70,8 +70,9 @@ KGpgTransactionPrivate::slotReadReady()
 #endif /* KGPG_DEBUG_TRANSACTIONS */
 
 		static const QString getBool = QLatin1String("[GNUPG:] GET_BOOL ");
-
-		if (line.startsWith(QLatin1String("[GNUPG:] USERID_HINT "))) {
+		if (m_parent->keyConsidered(line, QStringList())) {
+			// already handled by keyConsidered - skip the line
+		} else if (line.startsWith(QLatin1String("[GNUPG:] USERID_HINT "))) {
 			m_parent->addIdHint(line);
 		} else if (line.startsWith(QLatin1String("[GNUPG:] BAD_PASSPHRASE "))) {
 			// the MISSING_PASSPHRASE line comes first, in that case ignore a

transactions/kgpgchangedisable.h
@@ -35,7 +35,6 @@ public:
 
 protected:
 	bool preStart() override;
-	bool nextLine(const QString &line) override;
 };
 
 #endif // KGPGCHANGEDISABLE_H

transactions/kgpgsignkey.cpp
@@ -38,10 +38,6 @@ KGpgSignKey::~KGpgSignKey()
 bool
 KGpgSignKey::nextLine(const QString &line)
 {
-	if (keyConsidered(line, QStringList()))
-		// could be any private key, so just ignore them
-		return false;
-
 	switch (KGpgSignTransactionHelper::nextLine(line)) {
 	case KGpgSignTransactionHelper::handledFalse:
 		return false;

commit 2fe9414e2d4b14741ace4dd03781f36e436330f9

fix KEY_CONSIDERED lines always resulting in TS_MSG_SEQUENCE errors
The list of fingerprints the line was checked against was now always empty,
because I gave the bad advise to move the code into KGpgTransaction without
closely looking on the other places that called it.

For the time being we now simply ignore all KEY_CONSIDERED lines if the
transaction has not explicitely set a list of fingerprints to match against.

This is the first instance of a bug (and even a regression) found by the
testcases written by Kavinda Pitiduwa Gamage.

Fixes: 68fc9165947c054423ac600b11462294d56fa06e

transactions/kgpgchangedisable.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008,2009,2012 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ * Copyright (C) 2008,2009,2012,2018 Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
 /***************************************************************************
@@ -17,6 +17,7 @@ KGpgChangeDisable::KGpgChangeDisable(QObject *parent, const QString &keyid, cons
 	: KGpgEditKeyTransaction(parent, keyid, QString(), false)
 {
 	setDisable(disable);
+	setExpectedFingerprints( { keyid } );
 }
 
 KGpgChangeDisable::~KGpgChangeDisable()

transactions/kgpgdelkey.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008,2009,2012,2016,2017 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ * Copyright (C) 2008,2009,2012,2016,2017,2018 Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
 /***************************************************************************
@@ -35,6 +35,7 @@ KGpgDelKey::KGpgDelKey(QObject *parent, KGpgKeyNode *key)
 	, fingerprints(keyFingerprints(keys))
 {
 	setCmdLine();
+	setExpectedFingerprints(fingerprints);
 }
 
 KGpgDelKey::KGpgDelKey(QObject *parent, const KGpgKeyNode::List &keys)
@@ -43,6 +44,7 @@ KGpgDelKey::KGpgDelKey(QObject *parent, const KGpgKeyNode::List &keys)
 	, fingerprints(keyFingerprints(keys))
 {
 	setCmdLine();
+	setExpectedFingerprints(fingerprints);
 }
 
 KGpgDelKey::~KGpgDelKey()

transactions/kgpgtransaction.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008,2009,2010,2011,2012,2013,2014,2016 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ * Copyright (C) 2008,2009,2010,2011,2012,2013,2014,2016,2018 Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
 /***************************************************************************
@@ -315,6 +315,12 @@ KGpgTransaction::askPassphrase(const QString &message)
 }
 
 void
+KGpgTransaction::setExpectedFingerprints(const QStringList &fingerprints)
+{
+	d->m_expectedFingerprints = fingerprints;
+}
+
+void
 KGpgTransaction::setGnuPGHome(const QString &home)
 {
 	QStringList tmp(d->m_process->program());
@@ -396,18 +402,4 @@ KGpgTransaction::kill()
 void
 KGpgTransaction::newPassphraseEntered()
 {
-}
-
-bool KGpgTransaction::keyConsidered(const QString& line, const QStringList &fingerprints)
-{
-	if (!line.startsWith(QLatin1String("[GNUPG:] KEY_CONSIDERED ")))
-		return false;
-
-	const QStringList &parts = line.split(QLatin1Char(' '), QString::SkipEmptyParts);
-	if (parts.count() < 3)
-		setSuccess(KGpgTransaction::TS_MSG_SEQUENCE);
-	else if (!fingerprints.contains(parts[2], Qt::CaseInsensitive))
-		setSuccess(KGpgTransaction::TS_MSG_SEQUENCE);
-
-	return true;
 }

transactions/kgpgtransaction.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008,2009,2012,2013 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ * Copyright (C) 2008,2009,2012,2013,2018 Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
 /***************************************************************************
@@ -450,16 +450,12 @@ protected:
 	bool askPassphrase(const QString &message = QString());
 
 	/**
-	 * @brief handle if this a KEY_CONSIDERED line
-	 * @param line the line from GnuPG output
-	 * @param fingerprints the fingerprints of the expected keys
-	 * @returns if this is a KEY_CONSIDERED line
+	 * @brief set the fingerprints that are expected for this transaction
 	 *
-	 * In case this is a KEY_CONSIDERED line (i.e. the return value is true),
-	 * but either it was malformed or the given fingerprint does not match any
-	 * key in fingerprints, the success value will be set to TS_MSG_SEQUENCE.
+	 * This will skip any KEY_CONSIDERED messages from GnuPG that contain
+	 * any of the given fingerprints.
 	 */
-	bool keyConsidered(const QString &line, const QStringList &fingerprints);
+	void setExpectedFingerprints(const QStringList &fingerprints);
 };
 
 #endif // KGPGTRANSACTION_H

transactions/kgpgtransactionprivate.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009,2010,2012,2014,2016 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ * Copyright (C) 2009,2010,2012,2014,2016,2018 Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
 /***************************************************************************
@@ -70,7 +70,7 @@ KGpgTransactionPrivate::slotReadReady()
 #endif /* KGPG_DEBUG_TRANSACTIONS */
 
 		static const QString getBool = QLatin1String("[GNUPG:] GET_BOOL ");
-		if (m_parent->keyConsidered(line, QStringList())) {
+		if (keyConsidered(line)) {
 			// already handled by keyConsidered - skip the line
 		} else if (line.startsWith(QLatin1String("[GNUPG:] USERID_HINT "))) {
 			m_parent->addIdHint(line);
@@ -318,4 +318,19 @@ KGpgTransactionPrivate::processDone()
 #ifdef KGPG_DEBUG_TRANSACTIONS
 	qCDebug(KGPG_LOG_TRANSACTIONS) << this << "result:" << m_success;
 #endif /* KGPG_DEBUG_TRANSACTIONS */
+}
+
+bool KGpgTransactionPrivate::keyConsidered(const QString& line)
+{
+	if (!line.startsWith(QLatin1String("[GNUPG:] KEY_CONSIDERED ")))
+		return false;
+
+	const QStringList &parts = line.split(QLatin1Char(' '), QString::SkipEmptyParts);
+	if (parts.count() < 3)
+		m_parent->setSuccess(KGpgTransaction::TS_MSG_SEQUENCE);
+	else if (!m_expectedFingerprints.isEmpty() &&
+			!m_expectedFingerprints.contains(parts[2], Qt::CaseInsensitive))
+		m_parent->setSuccess(KGpgTransaction::TS_MSG_SEQUENCE);
+
+	return true;
 }

transactions/kgpgtransactionprivate.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008,2009,2012,2013 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ * Copyright (C) 2008,2009,2012,2013,2018 Rolf Eike Beer <kde@opensource.sf-tec.de>
  */
 
 /***************************************************************************
@@ -40,6 +40,7 @@ public:
 	bool m_chainingAllowed;
 
 	QStringList m_idhints;
+	QStringList m_expectedFingerprints;
 
 	QUrl m_overwriteUrl;	///< the file to overwrite or it's new name
 
@@ -73,6 +74,19 @@ public:
 	void write(const QByteArray &a);
 
 	static const QStringList &hintNames(void);
+
+	/**
+	 * @brief handle if this a KEY_CONSIDERED line
+	 * @param line the line from GnuPG output
+	 * @returns if this is a KEY_CONSIDERED line
+	 *
+	 * In case this is a KEY_CONSIDERED line (i.e. the return value is true),
+	 * but either it was malformed or the given fingerprint does not match any
+	 * key in m_expectedFingerprints, the success value will be set to TS_MSG_SEQUENCE.
+	 *
+	 * @see setExpectedIds
+	 */
+	bool keyConsidered(const QString &line);
 
 private:
 	void processDone();