KDE/kgpg

commit 565b6c3173745019539278d3bb9c05e524b776c3

use the configured config file and directory when generating keys in expert mode
BUG:428215

keysmanager.cpp
@@ -518,6 +518,8 @@ void KeysManager::slotGenerateKey()
 			else
 				gpg_args += QLatin1String(" --gen-key");
 
+			gpg_args += GPGProc::getGpgHomeArguments(gpgbin).join(QLatin1Char(' '));
+
 			QStringList args;
 			args << QLatin1String( "-e" )
 			     << gpg_args;

gpgproc.cpp
@@ -131,19 +131,7 @@ void GnupgBinary::setBinary(const QString &executable)
 			<< QLatin1String( "--no-tty" )
 			<< QLatin1String("--no-greeting");
 
-	if (!gpgConfigFile.isEmpty()) {
-		m_standardArguments << QLatin1String("--options")
-				<< gpgConfigFile;
-
-		// Check if the config file is in the default home directory
-		// of the binary. If it isn't add --homedir to command line also.
-		QString gpgdir = GPGProc::getGpgHome(executable);
-		gpgdir.chop(1);	// remove trailing '/' as QFileInfo returns string without it
-		QFileInfo confFile(gpgConfigFile);
-		if (confFile.absolutePath() != gpgdir)
-			m_standardArguments << QLatin1String("--homedir")
-					<< confFile.absolutePath();
-	}
+	m_standardArguments << GPGProc::getGpgHomeArguments(executable);
 
 	QStringList debugLevelArguments(QLatin1String("--debug-level"));
 	debugLevelArguments << QLatin1String("none");
@@ -406,4 +394,25 @@ QString GPGProc::getGpgHome(const QString &binary)
 	umask(mask);
 #endif
 	return gpgHome;
+}
+
+QStringList
+GPGProc::getGpgHomeArguments(const QString &binary)
+{
+	const QString gpgConfigFile = KGpgSettings::gpgConfigPath();
+
+	if (gpgConfigFile.isEmpty())
+		return {};
+
+	QStringList options{ QLatin1String("--options"), gpgConfigFile };
+
+	// Check if the config file is in the default home directory
+	// of the binary. If it isn't add --homedir to command line also.
+	QString gpgdir = GPGProc::getGpgHome(binary);
+	gpgdir.chop(1);	// remove trailing '/' as QFileInfo returns string without it
+	QFileInfo confFile(gpgConfigFile);
+	if (confFile.absolutePath() != gpgdir)
+		options << QLatin1String("--homedir") << confFile.absolutePath();
+
+	return options;
 }

gpgproc.h
@@ -142,6 +142,13 @@ public:
     static QString getGpgHome(const QString &binary);
 
     /**
+     * @brief get arguments to set the configuration file and home directory
+     * @param binary name or path to GnuPG binary
+     * @return GnuPG argument list
+     */
+    static QStringList getGpgHomeArguments(const QString &binary);
+
+    /**
      * @brief return a list of the public key algorithms GnuPG announces support for
      * @param binary name or path to GnuPG binary
      * @return list of algorithm names

commit 09b95f4d27b0475a0c8b937969125d31533317c2

add missing space between gnupg arguments when generating key in expert mode
Fixes: 565b6c3173745019539278d3bb9c05e524b776c3
BUG: 447611
FIXED-IN: 21.12.2

keysmanager.cpp
@@ -1,6 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2002 Jean-Baptiste Mardelle <bj@altern.org>
-    SPDX-FileCopyrightText: 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 Rolf Eike Beer <kde@opensource.sf-tec.de>
+    SPDX-FileCopyrightText: 2007-2022 Rolf Eike Beer <kde@opensource.sf-tec.de>
     SPDX-FileCopyrightText: 2011 Luis Ángel Fernández Fernández <laffdez@gmail.com>
     SPDX-FileCopyrightText: 2016 Andrius Štikonas <andrius@stikonas.eu>
     SPDX-License-Identifier: GPL-2.0-or-later
@@ -506,9 +506,9 @@ void KeysManager::slotGenerateKey()
 			const QString gpgbin = KGpgSettings::gpgBinaryPath();
 			QString gpg_args = gpgbin + QLatin1String(" --expert");
 			if (GPGProc::gpgVersion(GPGProc::gpgVersionString(gpgbin)) > 0x20100)
-				gpg_args += QLatin1String(" --full-gen-key");
+				gpg_args += QLatin1String(" --full-gen-key ");
 			else
-				gpg_args += QLatin1String(" --gen-key");
+				gpg_args += QLatin1String(" --gen-key ");
 
 			gpg_args += GPGProc::getGpgHomeArguments(gpgbin).join(QLatin1Char(' '));
 