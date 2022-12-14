KDE/kgpg

commit 55439df81fe368e1d26a054e600943783e738881

port deleting signatures to KGpgTransaction
svn path=/trunk/KDE/kdeutils/kgpg/; revision=1105765

keysmanager.cpp
@@ -110,6 +110,7 @@
 #include "kgpgtransactionjob.h"
 #include "kgpgsignkey.h"
 #include "kgpgsignuid.h"
+#include "kgpgdelsign.h"
 
 using namespace KgpgCore;
 
@@ -2218,21 +2219,24 @@ void KeysManager::delsignkey()
 	if (KMessageBox::questionYesNo(this, ask, QString(), KStandardGuiItem::del(), KStandardGuiItem::cancel()) != KMessageBox::Yes)
 		return;
 
-	KgpgInterface *delSignKeyProcess = new KgpgInterface();
-	connect(delSignKeyProcess, SIGNAL(delsigfinished(bool)), this, SLOT(delsignatureResult(bool)));
-	delSignKeyProcess->KgpgDelSignature(parentKey, uid, signID);
+	KGpgDelSign *delsig = new KGpgDelSign(this, nd->toSignNode());
+	connect(delsig, SIGNAL(done(int)), SLOT(delsignatureResult(int)));
+	delsig->start();
 }
 
-void KeysManager::delsignatureResult(bool success)
+void KeysManager::delsignatureResult(int success)
 {
-	if (success) {
+	sender()->deleteLater();
+
+	if (success == KGpgTransaction::TS_OK) {
 		KGpgNode *nd = iview->selectedNode()->getParentKeyNode();
 
 		while (!(nd->getType() & ITYPE_PAIR))
 			nd = nd->getParentKeyNode();
 		imodel->refreshKey(nd->toKeyNode());
-	} else
+	} else {
 		KMessageBox::sorry(this, i18n("Requested operation was unsuccessful, please edit the key manually."));
+	}
 }
 
 void KeysManager::slotedit()

CMakeLists.txt
@@ -50,6 +50,7 @@ set(kgpg_transaction_SRCS
    transactions/kgpgchangeexpire.cpp
    transactions/kgpgchangepass.cpp
    transactions/kgpgchangetrust.cpp
+   transactions/kgpgdelsign.cpp
    transactions/kgpgdeluid.cpp
    transactions/kgpgexport.cpp
    transactions/kgpgaddphoto.cpp

keysmanager.h
@@ -174,7 +174,7 @@ private slots:
     void preimportsignkey();
     void importallsignkey();
     void signatureResult(int success);
-    void delsignatureResult(bool);
+    void delsignatureResult(int success);
     void defaultAction(const QModelIndex &);
     void defaultAction(KGpgNode *);
     void slotDefaultAction();

kgpginterface.cpp
@@ -577,8 +577,6 @@ void KgpgInterface::readPublicKeysFin(GPGProc *p, const bool block)
 
 KgpgKeyList KgpgInterface::readSecretKeys(const QStringList &ids)
 {
-	m_partialline.clear();
-	m_ispartial = false;
 	m_secretlistkeys = KgpgKeyList();
 	m_secretkey = KgpgKey();
 	m_secretactivate = false;
@@ -751,87 +749,6 @@ void KgpgInterface::readPixmapFromProcess(KProcess *proc)
 	QFile::remove(url.path());
 	QDir dir;
 	dir.rmdir(url.directory());
-}
-
-// delete signature
-void KgpgInterface::KgpgDelSignature(const QString &keyID, const QString &uid, QString signKeyID)
-{
-	deleteSuccess = false;
-	step = 0;
-
-	signb = 0;
-	sigsearch = 0;
-	QList<int> signoff;
-
-	findSigns(keyID, QStringList(signKeyID), uid, &signoff);
-
-	if (signoff.count() == 0)
-		return;
-
-	signb = signoff.at(0);
-
-	GPGProc *conprocess = new GPGProc(this);
-	*conprocess << "--command-fd=0" << "--status-fd=1" << "--edit-key" << keyID << "uid" << uid << "delsig";
-	connect(conprocess, SIGNAL(readReady()), SLOT(delsigprocess()));
-	connect(conprocess, SIGNAL(processExited()), SLOT(delsignover()));
-	conprocess->start();
-}
-
-void KgpgInterface::delsigprocess()
-{
-	GPGProc *p = qobject_cast<GPGProc *>(sender());
-	QString required;
-
-	while (p->readln(required, true) >= 0) {
-		if (required.contains("keyedit.delsig")){
-			if ((sigsearch == signb) && (step == 0)) {
-				p->write("Y\n");
-				step = 1;
-			} else
-				p->write("n\n");
-			sigsearch++;
-			required.clear();
-		} else if ((step == 1) && required.contains("keyedit.prompt")) {
-			p->write("save\n");
-			required.clear();
-			deleteSuccess = true;
-		} else if (required.contains("GET_LINE")) {
-			p->write("quit\n");
-			deleteSuccess = false;
-		}
-	}
-}
-
-void KgpgInterface::delsignover()
-{
-	sender()->deleteLater();
-	emit delsigfinished(deleteSuccess);
-}
-
-void KgpgInterface::findSigns(const QString &keyID, const QStringList &ids, const QString &uid, QList<int> *res)
-{
-	GPGProc *listproc = new GPGProc(this);
-	*listproc << "--status-fd=1";
-	*listproc << "--with-colons" << "--list-sigs" << "--fixed-list-mode" << keyID;
-	listproc->start();
-	listproc->waitForFinished(-1);
-
-	QString line;
-	int curuid = 0;
-	int signs = 0;
-	int tgtuid = uid.toInt();
-
-	while (listproc->readln(line, true) >= 0) {
-		if (line.startsWith(QLatin1String("sig:")) && (tgtuid == curuid)) {
-			if (ids.contains(line.section(':', 4, 4), Qt::CaseInsensitive))
-				*res << signs;
-			signs++;
-		} else if (line.startsWith(QLatin1String("uid:"))) {
-			curuid++;
-		}
-	}
-
-	delete listproc;
 }
 
 #include "kgpginterface.moc"

kgpginterface.h
@@ -145,58 +145,15 @@ private:
 
 /*******************************************************/
 
-
-/********************************************************/
-public slots:
-
-    /**
-     * Key signature deletion function
-     * @param keyID the ID of the key
-     * @param uid the user id that is signed
-     * @param signKeyID the ID of the signature key
-     */
-    void KgpgDelSignature(const QString &keyID, const QString &uid, QString signKeyID);
-
-private slots:
-    /**
-     * Reads output of the delete signature process
-     */
-    void delsigprocess();
-
-    /**
-     * Checks output of the delete signature process
-     */
-    void delsignover();
-
-    /**
-     * Finds the offset of the given signatures to a uid
-     */
-    void findSigns(const QString &keyID, const QStringList &ids, const QString &uid, QList<int> *res);
-
-signals:
-    /**
-     *  true if key signature deletion successful, false on error.
-     */
-    void delsigfinished(bool);
-
 private:
     // Globals private
     int m_success;
-    QString m_partialline;
-    bool m_ispartial;
-    QString message;
     QString log;
 
     /**
      * @internal structure for communication
      */
     QString output;
-
-    bool deleteSuccess;
-
-    int step;
-    int signb;
-    int sigsearch;
 };
 
 #endif // KGPGINTERFACE_H

transactions/kgpgdelsign.cpp
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2010 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ */
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "kgpgdelsign.h"
+
+#include <QStringList>
+
+#include "kgpgitemnode.h"
+#include "gpgproc.h"
+
+KGpgDelSign::KGpgDelSign(QObject *parent, const KGpgSignNode::List &signids)
+	: KGpgUidTransaction(parent, signids.at(0)->getParentKeyNode()->getId())
+{
+	addArgument("delsig");
+
+	const QStringList args = getProcess()->program();
+
+	// If we run with --no-tty GnuPG will not tell which sign it is currently
+	// asking to remove :(
+	int ntty = args.indexOf(QLatin1String("--no-tty"));
+	if (ntty >= 0)
+		replaceArgument(ntty, QLatin1String("--with-colons"));
+	else
+		insertArgument(1, "--with-colons");
+
+	if (signids.at(0)->getParentKeyNode()->getType() & KgpgCore::ITYPE_PUBLIC)
+		setUid("1");
+	else
+		setUid(signids.at(0)->getParentKeyNode()->getId());
+
+	foreach (const KGpgSignNode *snode, signids) {
+		Q_ASSERT(signids.at(0)->getParentKeyNode() == snode->getParentKeyNode());
+	}
+
+	setSignIds(signids);
+}
+
+KGpgDelSign::KGpgDelSign(QObject* parent, KGpgSignNode *signid)
+	: KGpgUidTransaction(parent, signid->getParentKeyNode()->getId())
+{
+	addArgument("delsig");
+	insertArgument(1, "--with-colons");
+
+	if (signid->getParentKeyNode()->getType() & KgpgCore::ITYPE_PUBLIC)
+		setUid("1");
+	else
+		setUid(signid->getParentKeyNode()->getId());
+
+	setSignId(signid);
+}
+
+
+KGpgDelSign::~KGpgDelSign()
+{
+}
+
+KGpgSignNode::List KGpgDelSign::getSignIds(void) const
+{
+	return m_signids;
+}
+
+void KGpgDelSign::setSignId(KGpgSignNode* keyid)
+{
+	m_signids.clear();
+	m_signids << keyid;
+}
+
+void KGpgDelSign::setSignIds(const KGpgSignNode::List &keyids)
+{
+	m_signids = keyids;
+}
+
+bool
+KGpgDelSign::nextLine(const QString &line)
+{
+	if (line.startsWith(QLatin1String("sig:"))) {
+		m_cachedid = line;
+		return false;
+	} else if (line.startsWith(QLatin1String("[GNUPG:] "))) {
+		return standardCommands(line);
+	} else {
+		// GnuPG will tell us a bunch of stuff because we are not in
+		// --no-tty mode but we don't care.
+		return false;
+	}
+}
+
+KGpgTransaction::ts_boolanswer
+KGpgDelSign::boolQuestion(const QString &line)
+{
+	if (line.startsWith(QLatin1String("keyedit.delsig."))) {
+		const QStringList parts = m_cachedid.split(':');
+
+		if (parts.count() < 7)
+			return KGpgTransaction::BA_NO;
+
+		const QString &sigid = parts[4];
+		const int snlen = sigid.length();
+		KGpgSignNode *signode = NULL;
+
+		foreach (KGpgSignNode *snode, m_signids) {
+			if (sigid == snode->getId().right(snlen)) {
+				signode = snode;
+				break;
+			}
+		}
+
+		if (signode == NULL)
+			return KGpgTransaction::BA_NO;
+
+		const QDateTime creation = QDateTime::fromTime_t(parts[5].toUInt());
+		if (creation != signode->getCreation())
+			return KGpgTransaction::BA_NO;
+
+		QDateTime sigexp;
+		if (!parts[6].isEmpty() && (parts[6] != QLatin1String("0")))
+			sigexp = QDateTime::fromTime_t(parts[6].toUInt());
+		if (sigexp != signode->getExpiration())
+			return KGpgTransaction::BA_NO;
+
+		m_signids.removeOne(signode);
+		return KGpgTransaction::BA_YES;
+	} else {
+		return KGpgTransaction::boolQuestion(line);
+	}
+}

transactions/kgpgdelsign.h
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2010 Rolf Eike Beer <kde@opensource.sf-tec.de>
+ */
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KGPGDELSIGN_H
+#define KGPGDELSIGN_H
+
+#include <QObject>
+#include <QString>
+
+#include "KGpgSignNode.h"
+#include "kgpguidtransaction.h"
+
+/**
+ * @brief delete signatures from user ids
+ */
+class KGpgDelSign: public KGpgUidTransaction {
+	Q_OBJECT
+
+public:
+	/**
+	 * @brief construct a new transaction to delete signatures
+	 * @param parent object that own the transaction
+	 * @param signids list of signature ids to remove
+	 *
+	 * All members of signids need to have the same parent node, i.e.
+	 * they not even may be signatures to different uids of the same key.
+	 */
+	KGpgDelSign(QObject *parent, const KGpgSignNode::List &signids);
+	/**
+	 * @brief construct a new transaction to delete signatures
+	 * @param parent object that own the transaction
+	 * @param signid signature to delete
+	 */
+	KGpgDelSign(QObject *parent, KGpgSignNode *signid);
+
+	virtual ~KGpgDelSign();
+
+	/**
+	 * @brief set the ids to delete
+	 * @param signids fingerprints of the signatures to delete
+	 *
+	 * This will replace all previously set signature ids.
+	 */
+	void setSignIds(const KGpgSignNode::List &keyids);
+	/**
+	 * @brief set the id to delete
+	 * @param keyid fingerprint of the signatures to delete
+	 * @overload
+	 *
+	 * This will replace all previously set signature ids.
+	 */
+	void setSignId(KGpgSignNode *keyid);
+	/**
+	 * @brief return the signature ids to delete
+	 */
+	KGpgSignNode::List getSignIds(void) const;
+
+protected:
+	virtual bool nextLine(const QString &line);
+	virtual ts_boolanswer boolQuestion(const QString &line);
+
+private:
+	Q_DISABLE_COPY(KGpgDelSign)
+	KGpgDelSign(); // = delete C++0x
+
+	KGpgSignNode::List m_signids;	///< the list of ids to delete
+	QString m_cachedid;	///< the next id GnuPG will ask to delete
+};
+
+#endif // KGPGDELSIGN_H

commit d02f1be3186db5766c86eee95da14f8124936d29

remove write-only local variable when deleting uids
Fixes: 55439df81fe368e1d26a054e600943783e738881

keysmanager.cpp
@@ -2177,19 +2177,16 @@ void KeysManager::delsignkey()
 	if (nd == nullptr)
 		return;
 
-	QString uid;
 	QString parentKey;
 
 	KGpgExpandableNode *parent = nd->getParentKeyNode();
 	switch (parent->getType()) {
 	case ITYPE_PAIR:
 	case ITYPE_PUBLIC:
-		uid = QLatin1Char( '1' );
 		parentKey = parent->getId();
 		break;
 	case ITYPE_UID:
 	case ITYPE_UAT:
-		uid = parent->getId();
 		parentKey = parent->getParentKeyNode()->getId();
 		break;
 	default: