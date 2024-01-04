c53a650b28332ce02efa8b83b2a291275b51bb82
Albert Astals Cid <aacid@kde.org>
2024-01-04 22:00:11 +0100

    There's no tips in KF6-land
    Followup of 4ca7d1db31111af7af551fa3d0e0a5bd157fb966

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-$PREPARETIPS > tips.cpp
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT *.h *.cpp core/*.cpp editor/*.cpp model/*.cpp transactions/*.cpp -o $podir/kgpg.pot
 rm -f tips.cpp
